91b7678cae7749497525ce55eda4730571f3c67e
Rouslan Placella <rouslan@placella.com>
2015-03-07 18:15:38 +0000

    Return full content from rcon command

--- backend/phpQuake3.php
+++ backend/phpQuake3.php
@@ -34,7 +34,8 @@ class q3query {
 
     public function __construct($address, $port, $password) {
         $this->cmd = str_repeat(chr(255), 4);
-        $this->fp = fsockopen("udp://$address", $port, $this->errno, $this->errstr, 30);
+        $this->fp = fsockopen("udp://$address", $port, $this->errno, $this->errstr, 2);
+        socket_set_timeout($this->fp, 2);
         $this->rconpassword = $password;
     }
 
@@ -72,37 +73,38 @@ class q3query {
             $response['map'] = substr($map, 5);
             array_shift($result); // 2nd line : col headers
             array_shift($result); // 3rd line : -- ------ ----
-            array_pop($result);
-            array_pop($result); // two empty lines at the end
             $response['players'] = array();
             foreach ($result as $line) {
                 $player = $line;
-                preg_match_all(
-                    "/^\s*(\d+)\s*(\d+)\s*(\d+)(.*?)(\d*)\s*(\S*)\s*(\d*)\s*(\d*)\s*$/",
-                    $player,
-                    $out
-                );
-                $num = $out[1][0];
-                $score = $out[2][0];
-                $ping = $out[3][0];
-                $name = trim($out[4][0]);
-                $lastmsg = $out[5][0];
-                $address = $out[6][0];
-                if ($address != 'bot') {
-                    $address = preg_replace("/:.*$/", "", $address);
+                if (
+                    preg_match_all(
+                        "/^\s*(\d+)\s*(\d+)\s*(\d+)(.*?)(\d*)\s*(\S*)\s*(\d*)\s*(\d*)\s*$/",
+                        $player,
+                        $out
+                    )
+                ) {
+                    $num = $out[1][0];
+                    $score = $out[2][0];
+                    $ping = $out[3][0];
+                    $name = trim($out[4][0]);
+                    $lastmsg = $out[5][0];
+                    $address = $out[6][0];
+                    if ($address != 'bot') {
+                        $address = preg_replace("/:.*$/", "", $address);
+                    }
+                    $qport = $out[7][0];
+                    $rate = $out[8][0];
+                    $response['players'][] = array(
+                        'num' => intval($num),
+                        'score' => intval($score),
+                        'ping' => intval($ping),
+                        'name' => $name,
+                        'lastmsg' => $lastmsg,
+                        'address' => $address,
+                        'qport' => $qport,
+                        'rate' => $rate
+                    );
                 }
-                $qport = $out[7][0];
-                $rate = $out[8][0];
-                $response['players'][] = array(
-                    'num' => intval($num),
-                    'score' => intval($score),
-                    'ping' => intval($ping),
-                    'name' => $name,
-                    'lastmsg' => $lastmsg,
-                    'address' => $address,
-                    'qport' => $qport,
-                    'rate' => $rate
-                );
             }
         } else {
             $response = null;
@@ -111,18 +113,16 @@ class q3query {
     }
 
     private function get_response($timeout) {
-        $s = '';
-        $bang = time() + $timeout;
-        while (!strlen($s) and time() < $bang) {
-            usleep(500000);
-            $s = $this->recv();
+        $data = '';
+        while ($d = $this->recv()) {
+            if (substr($d, 0, 4) === $this->cmd) {
+                $data .= preg_replace("/^....print\n/", "", $d);
+            } else {
+                return null;
+            }
         }
-        if (substr($s, 0, 4) === $this->cmd) {
-            $s = preg_replace("/^....print\n/", "", $s);
-            return $this->stripcolors($s);
-        } else {
-            return null;
-        }
+        fclose($this->fp);
+        return $this->stripcolors($data);
     }
 
     private function send($string) {
